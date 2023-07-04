ad464a0b564d28c40526e39dd2e594b6845a0fdd
Sachin rajput <sachin108rajput@hotmail.com>
2023-07-05 04:39:34 +0530

    5th July

---
+++ date_calculation_service/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DateCalculationServiceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'date_calculation_service'
