2b1243999dc9fc19be05054c879a6aba5cbd99f6
Sachin rajput <sachin108rajput@hotmail.com>
2023-07-07 12:35:06 +0530

    mysql

--- Employee_Celebrations_Tracker/settings.py
+++ Employee_Celebrations_Tracker/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'Employee_Celebrations_Tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'springdb',
+        'USER': 'admin',
+        'PASSWORD': 'r3Eh8C7FP2pxK2x',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
